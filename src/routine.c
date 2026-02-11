a955d45686422bf3d9dada8daeb7a6f9b38354b7
Tomás Andrade <tomas.andrade2043@gmail.com>
2026-02-11 14:09:05 +0000

    Starting philo routine, need to fix deadlock in philo_routine()

---
+++ src/routine.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toandrad <toandrad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 12:05:38 by toandrad          #+#    #+#             */
+/*   Updated: 2026/02/11 14:00:09 by toandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+void	print_status(t_philo *philo, char *status)
+{
+	long	timestamp;
+
+	pthread_mutex_lock(&philo->data->death_mutex);
+	if (philo->data->death_flag)
+	{
+		pthread_mutex_unlock(&philo->data->death_mutex);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->data->death_mutex);
+	timestamp = get_time() - philo->data->start_time;
+	pthread_mutex_lock(&philo->data->print_mutex);
+	printf("%ld %d %s\n", timestamp, philo->id, status);
+	pthread_mutex_unlock(&philo->data->print_mutex);
+}
+
+void	pick_up_forks(t_philo *philo)
+{
+	int	first_fork;
+	int	second_fork;
+
+	if (philo->left_fork < philo->right_fork)
+	{
+		first_fork = philo->left_fork;
+		second_fork = philo->right_fork;
+	}
+	else
+	{
+		first_fork = philo->right_fork;
+		second_fork = philo->left_fork;
+	}
+	pthread_mutex_lock(&philo->data->forks[first_fork]);
+	print_status(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->data->forks[second_fork]);
+	print_status(philo, "has taken a fork");
+}
+
+void	put_down_forks(t_philo *philo)
+{
+	pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
+	pthread_mutex_unlock(&philo->data->forks[philo->right_fork]);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	pthread_mutex_lock(&philo->data->death_mutex);
+	while (!philo->data->death_flag)
+	{
+		print_status(philo, "is thinking");
+		pick_up_forks(philo);
+		pthread_mutex_lock(&philo->meal_mutex);
+		print_status(philo, "is eating");
+		philo->last_meal_time = get_time();
+		ft_usleep(philo->data->time_to_eat);
+		philo->meals_eaten++;
+		pthread_mutex_unlock(&philo->meal_mutex);
+		put_down_forks(philo);
+		print_status(philo, "is sleeping");
+		ft_usleep(philo->data->time_to_sleep);
+	}
+	pthread_mutex_unlock(&philo->data->death_mutex);
+	return (NULL);
+}
